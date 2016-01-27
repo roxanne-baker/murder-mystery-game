5e45dd6b63130ec0f87c0464d81abf56d7e58d6e
Roxanne Baker <reb36@duke.edu>
2016-01-27 02:14:05 -0500

    Update NPC.java as part of masterpiece

--- compsci308_game_reb36/src/javaFX/NPC.java
+++ compsci308_game_reb36/src/javaFX/NPC.java
@@ -5,20 +5,43 @@ import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
 public class NPC extends Combatant {
-
+	// This entire file is part of my masterpiece
+	// ROXANNE BAKER
+	/**
+	 * I feel this code is well written because
+	 * it contains short functions, each with a single
+	 * purpose that is obvious from its name
+	 * There is no duplicated code here
+	 * 
+	 * This class can be used to create an NPC
+	 * Subclasses allow the NPC to move
+	 * 
+	 * Dialogue represents the text that shows
+	 * when player interacts with NPC
+	 * 
+	 * startX and startY represent the initial
+	 * x and y coordinates of the NPC
+	 * 
+	 * the "speed" variables dictate the speeds
+	 * for the NPC when moving in certain directions
+	 * 
+	 * timeStopped represents the amount of time that
+	 * the NPC is stopped for in between movements
+	 * 
+	 * Since player can fight NPC, it extends the
+	 * Combatant superclass.  Stats for the NPC
+	 * are set in the constructor.
+	 */
 	private NPCText dialogue;
 	private double startX;
 	private double startY;
-	
 	private double speedX1;
 	private double speedY1;
 	private double speedX2;
 	private double speedY2;
-	
 	private double timeStopped = 0;
 	
-	public NPC() {
-	}
+	public NPC() {}
 	
 	public NPC(String overworldImage, String battleImage, double x, double y) {
 		this.setImage(overworldImage);
@@ -35,60 +58,85 @@ public class NPC extends Combatant {
 		setCurrHP(getMaxHP());
 	}
 	
-	public NPC(Image filename, double x, double y) {
-		this.setImage(filename);
-		this.setPosition(x, y);	
-		setStartX(x);
-		setStartY(y);
-	}
-	
+	/**
+	 * Empty function because in the superclass
+	 * NPCs do not move.
+	 * 
+	 * Overwritten in subclasses.
+	 * @param time
+	 */
 	public void move(double time) {};
 	
-	public void setDialogue(String message) {
+	/**
+	 * Sets the dialogue of the NPC
+	 * and the name of the NPC
+	 * 
+	 * Assumes text is of format "NAME: text"
+	 * @param message
+	 */
+	public void setDialogueAndName(String message) {
 		int colonLoc = message.indexOf(": ");
 		setName(message.substring(0, colonLoc));
 		dialogue = new NPCText(message);
 	}
 	
-	public void setFirstDirection(String fstDirn, double speed) {		
-		if (fstDirn == "LEFT") {
-			setSpeedX1(-speed);
-			setSpeedY1(0);
+	/**
+	 * Sets direction that the NPC is moving
+	 * @param setSpeed1
+	 * @param direction
+	 * @param speed
+	 */
+	protected void setDirection(boolean setSpeed1, String direction, double speed) {
+		if (direction == "LEFT") {
+			setSpeedLeg1OrLeg2(setSpeed1, true, -speed);
+			setSpeedLeg1OrLeg2(setSpeed1, false, 0);
 		}
-		else if (fstDirn == "RIGHT") {
-			setSpeedX1(speed);
-			setSpeedY1(0);
+		else if (direction == "RIGHT") {
+			setSpeedLeg1OrLeg2(setSpeed1, true, speed);
+			setSpeedLeg1OrLeg2(setSpeed1, false, 0);
 		}
-		else if (fstDirn == "UP") {
-			setSpeedX1(0);
-			setSpeedY1(-speed);
+		else if (direction == "UP") {
+			setSpeedLeg1OrLeg2(setSpeed1, true, 0);
+			setSpeedLeg1OrLeg2(setSpeed1, false, -speed);
 		}
-		else if (fstDirn == "DOWN") {
-			setSpeedX1(0);
-			setSpeedY1(speed);
+		else if (direction == "DOWN") {
+			setSpeedLeg1OrLeg2(setSpeed1, true, 0);
+			setSpeedLeg1OrLeg2(setSpeed1, false, speed);
+		}		
+	}
+	
+	/**
+	 * Sets the speed of the corresponding leg
+	 * when given which leg is given and whether
+	 * the "x" or "y" speed is being set
+	 * @param setSpeed1
+	 * @param setXSpeed
+	 * @param speedValue
+	 */
+	private void setSpeedLeg1OrLeg2(boolean setSpeed1, boolean setXSpeed, double speedValue) {
+		if (setSpeed1 && setXSpeed) {
+			setSpeedX1(speedValue);
+		}
+		else if (setSpeed1 && !setXSpeed) {
+			setSpeedY1(speedValue);
+		}
+		else if (!setSpeed1 && setXSpeed) {
+			setSpeedX2(speedValue);
+		}
+		else if (!setSpeed1 && !setXSpeed) {
+			setSpeedY2(speedValue);
 		}
 	}
 	
-	public void setSecondDirection(String sndDirn, double speed) {		
-		if (sndDirn == "LEFT") {
-			setSpeedX2(-speed);
-			setSpeedY2(0);
-		}
-		else if (sndDirn == "RIGHT") {
-			setSpeedX2(speed);
-			setSpeedY2(0);
-		}
-		else if (sndDirn == "UP") {
-			setSpeedX2(0);
-			setSpeedY2(-speed);
-		}
-		else if (sndDirn == "DOWN") {
-			setSpeedX2(0);
-			setSpeedY2(speed);
-		}
-	}
-	
-	public void stopAndGo(double passedTime, double waitTime, double nextXSpeed, double nextYSpeed) {
+	/**
+	 * Causes the NPC to either stop or go
+	 * based on the wait time and timeStopped
+	 * @param passedTime
+	 * @param waitTime
+	 * @param nextXSpeed
+	 * @param nextYSpeed
+	 */
+	protected void stopAndGo(double passedTime, double waitTime, double nextXSpeed, double nextYSpeed) {
 		if (getTimeStopped() <= waitTime) {
 			this.setVelocity(0, 0);
 			setTimeStopped(getTimeStopped() + passedTime);
@@ -99,7 +147,16 @@ public class NPC extends Combatant {
 		}
 	}
 	
-	public boolean getCond(double speedX, double speedY, double legLength) {
+	/**
+	 * Determines one of the conditions
+	 * for "atEndOfLeg" method
+	 * @param speedX
+	 * @param speedY
+	 * @param legLength
+	 * @return whether or not condition
+	 * for "atEndOfLeg" method is true/false
+	 */
+	protected boolean getCond(double speedX, double speedY, double legLength) {
 		if (speedX < 0) {
 			return this.getLeftX() >= getStartX()+legLength;
 		}
@@ -115,7 +172,19 @@ public class NPC extends Combatant {
 		return false;
 	}
 	
-	public boolean atEndOfLeg(double speedX1, double speedY1, double legLength1, double speedX2, double speedY2, double legLength2) {
+	/**
+	 * 
+	 * @param speedX1
+	 * @param speedY1
+	 * @param legLength1
+	 * @param speedX2
+	 * @param speedY2
+	 * @param legLength2
+	 * @return whether or not the NPC
+	 * is at the end of its current
+	 * leg of movement
+	 */
+	protected boolean atEndOfLeg(double speedX1, double speedY1, double legLength1, double speedX2, double speedY2, double legLength2) {
 		boolean firstCond = false;
 		boolean endCond = false;
 		firstCond = getCond(speedX1, speedY1, legLength1);
@@ -125,10 +194,6 @@ public class NPC extends Combatant {
 
 	public NPCText getDialogue() {
 		return dialogue;
-	}
-
-	public void setDialogue(NPCText dialogue) {
-		this.dialogue = dialogue;
 	}
 
 	public double getStartX() {
@@ -186,5 +251,4 @@ public class NPC extends Combatant {
 	public void setTimeStopped(double timeStopped) {
 		this.timeStopped = timeStopped;
 	}
-	
 }
